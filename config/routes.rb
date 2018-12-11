9eae7868f6200bbd037c321e05881fd0c6d5501d
Terrance D Williams <Terrance.D.Williams1976@gmail.com>
2018-12-11 14:13:26 -0500

    Hello

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ devise_for :users
 
   
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with "rake routes".hhhh
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
